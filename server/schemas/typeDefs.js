e1b40c7f555e344d86f05d176cd1df72f5eae3b4
J Scott Rumptz <jscott@rumptz.tech>
2021-04-18 09:28:35 -0500

    updated resolvers for adding and saving books

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -40,7 +40,7 @@ const typeDefs = gql`
     type Mutation {
         login(email: String!, password: String!): Auth
         addUser(username: String!, email: String!, password: String!): Auth
-        saveBook(savedBooks: BookInput!): User
+        saveBook(book: BookInput!): User
         removeBook(bookId: String!): User
     }
     
